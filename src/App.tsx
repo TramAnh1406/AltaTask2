cb52c385bd1339d9f84c8a5277814aa526aa201e
Âu Duy Thông <auduythong1610@gmail.com>
2021-11-04 15:12:08 +0700

    Thông update css main

--- src/App.tsx
+++ src/App.tsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import IndexMenu from "./components/Menu/Menu";
 import User from "./components/TopUser/User";
+import ChatBody from "./components/QuestionAndAnswer/chatBody/ChatBody";
 
 export default class App extends Component {
   render() {
@@ -16,6 +17,7 @@ export default class App extends Component {
             <main>
               <User />
               <div className="content">
+              <Route path="/question-answer" component={ChatBody}/>                
               </div>
             </main>
           </div>
